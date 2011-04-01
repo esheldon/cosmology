8dfddd04b4f8015af7fde600e0f14fe72cd49346
Erin Sheldon <erin.sheldon@gmail.com>
2011-04-01 00:01:14 -0400

    make it so installing ups is completely separate

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import sys
 import os
 from numpy.distutils.misc_util import Configuration
 from numpy.distutils.core import setup
+import distutils.config
 
 from optparse import OptionParser
 parser=OptionParser(__doc__)
 parser.add_option("--prefix",default=None, help="the prefix")
+
 
 def create_ups(prefix):
     import distutils.config
@@ -30,31 +32,39 @@ envPrepend(PYTHONPATH,${PRODUCT_DIR}/%s)
         sys.stdout.write("Creating ups dir: %s\n" % upsdir)
         os.makedirs(upsdir)
 
-    upsname=os.path.join(upsdir,'cosmology.table')
+    upsname=os.path.join(upsdir,'fimage.table')
     sys.stdout.write('Writing ups table file: %s\n' % upsname)
     upsfile=open(upsname, 'w')
     upsfile.write(upstext)
     upsfile.close()
     del upsfile
 
+
+
+
 def configuration(parent_package='',top_path=None):
     config = Configuration('cosmology',parent_package,top_path)
+
     config.add_extension('_cosmolib', ['cosmolib.f90'])
+
     return config
 
 if __name__ == "__main__":
-    with_ups=False
-    try:
-        ind = sys.argv.index('with_ups')
-        del sys.argv[ind]
-        with_ups=True
-    except:
-        pass
-    setup(configuration=configuration)
-
-    # only install ups if not default prefix
-    if with_ups:
+    if sys.argv.count("install_ups") != 0:
+        # this is special, just install ups
+        # I did it this way because a command class can *only*
+        # install under the main package dir, not prefix/ups for example.
+        # and setting config_fc --fcompiler=gnu95 disallows the with_ups
+        # option for some reason
         options, args = parser.parse_args(sys.argv[1:])
         prefix=options.prefix
         if prefix is not None:
             create_ups(prefix)
+        else:
+            raise ValueError("can only install ups when prefix is set")
+
+        sys.exit(0)
+
+    setup(configuration=configuration)
+
+
